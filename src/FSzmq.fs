73bc5b87f5302051ae679274c772d11d35b47078
joranvar <bart.post@gmail.com>
2016-02-27 18:01:47 +0100

    Modularize objects

--- src/FSzmq.fs
+++ src/FSzmq.fs
@@ -2,21 +2,28 @@ namespace FSzmq
 
 module Message =
   type T = byte array
-  type BinaryFormatter = System.Runtime.Serialization.Formatters.Binary.BinaryFormatter
-  type MemoryStream = System.IO.MemoryStream
+
+  module MemoryStream =
+    type T = System.IO.MemoryStream
+    let create () : T = new T ()
+    let ofArray (arr:byte array) : T = new T (arr)
+    let toArray (t:T) : byte array = t.ToArray ()
+
+  module BinaryFormatter =
+    type T = System.Runtime.Serialization.Formatters.Binary.BinaryFormatter
+    let create () : T = T ()
+    let serialize (x:obj) (ms:MemoryStream.T) (t:T) = t.Serialize (ms, x)
+    let deserialize (ms:MemoryStream.T) (t:T) = t.Deserialize ms
 
   let ofT<'t> (x:'t) : T =
     if box x = null then [||]
     else
-      let binFormatter = new BinaryFormatter()
-      use stream = new MemoryStream()
-      binFormatter.Serialize(stream, x)
-      stream.ToArray()
+      use stream = MemoryStream.create ()
+      BinaryFormatter.create () |> BinaryFormatter.serialize x stream
+      MemoryStream.toArray stream
 
   let toT<'t> (t:T) : 't =
     if Array.length t = 0 then unbox null
     else
-      let binFormatter = new BinaryFormatter()
-
-      use stream = new MemoryStream(t)
-      binFormatter.Deserialize(stream) :?> 't
+      use stream = MemoryStream.ofArray t
+      BinaryFormatter.create () |> BinaryFormatter.deserialize stream |> unbox
