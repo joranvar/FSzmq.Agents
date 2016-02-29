45594571c5d2efd14abdc1eba42738c3887fb2a7
joranvar <bart.post@gmail.com>
2016-02-29 05:40:05 +0100

    Move Machine, Network and Connection to one module
    This centralizes the implementation of the string conversion.

--- src/FSzmq.fs
+++ src/FSzmq.fs
@@ -39,14 +39,24 @@ module Context =
   type T = fszmq.Context
   let create () = new T ()
 
-type Machine = | Localhost
-type Network = | Localhost
+module Connection =
+  type Network = | LocalhostNetwork | InterNetwork
+  type Machine = | Localhost | Other of System.Net.IPAddress
+  type T = | Network of Network * int | Machine of Machine * int
+  let localIP =
+    let localHost = System.Net.Dns.GetHostName()
+    System.Net.Dns.GetHostEntry(localHost).AddressList
+    |> Array.find (fun x -> x.AddressFamily.ToString() = "InterNetwork")
+  let toString = function
+    | Machine (Localhost, p)
+    | Network (LocalhostNetwork, p)-> sprintf "tcp://127.0.0.1:%d" p
+    | Machine (Other ip, p)-> sprintf "tcp://%A:%d" ip p
+    | Network (InterNetwork, p)-> sprintf "tcp://%A:%d" localIP p
 
 module Agent =
   type T<'t> = MailboxProcessor<'t>
   module Socket =
     type S = fszmq.Socket * System.Threading.SynchronizationContext
-    type Connection = | Network of Network * int | Machine of Machine * int
     type Type = | Pull | Push | Sub of Message.T list | Pub | Req | Rep
 
     let create (t:Type) (c:Context.T) =
@@ -59,17 +69,18 @@ module Agent =
            | Req -> fszmq.Context.req
            | Rep -> fszmq.Context.rep
        , System.Threading.SynchronizationContext.Current
-    let connect (c:Connection) (s:S) =
+    let connect (c:Connection.T) (s:S) =
+      let address = c |> Connection.toString
       c |> function
-        | Network (n, p) -> fszmq.Socket.bind (fst s) (sprintf "tcp://127.0.0.1:%d" p)
-        | Machine (m, p) -> fszmq.Socket.connect (fst s) (sprintf "tcp://127.0.0.1:%d" p)
+        | Connection.Network (n, p) -> fszmq.Socket.bind (fst s) address
+        | Connection.Machine (m, p) -> fszmq.Socket.connect (fst s) address
       s
-    let pull (c:Context.T) (m:Machine) (port:int) () = c |> create Pull |> connect (Machine (m, port))
-    let push (c:Context.T) (n:Network) (port:int) () = c |> create Push |> connect (Network (n, port))
-    let subscribe (c:Context.T) (m:Machine) (port:int) () = c |> create (Sub [ ""B ]) |> connect (Machine (m, port))
-    let publish (c:Context.T) (n:Network) (port:int) () = c |> create Pub |> connect (Network (n, port))
-    let request (c:Context.T) (m:Machine) (port:int) () = c |> create Req |> connect (Machine (m, port))
-    let reply (c:Context.T) (n:Network) (port:int) () = c |> create Rep |> connect (Network (n, port))
+    let pull (c:Context.T) (m:Connection.Machine) (port:int) () = c |> create Pull |> connect (Connection.Machine (m, port))
+    let push (c:Context.T) (n:Connection.Network) (port:int) () = c |> create Push |> connect (Connection.Network (n, port))
+    let subscribe (c:Context.T) (m:Connection.Machine) (port:int) () = c |> create (Sub [ ""B ]) |> connect (Connection.Machine (m, port))
+    let publish (c:Context.T) (n:Connection.Network) (port:int) () = c |> create Pub |> connect (Connection.Network (n, port))
+    let request (c:Context.T) (m:Connection.Machine) (port:int) () = c |> create Req |> connect (Connection.Machine (m, port))
+    let reply (c:Context.T) (n:Connection.Network) (port:int) () = c |> create Rep |> connect (Connection.Network (n, port))
 
     let send (s:S) msg = async { do! Async.SwitchToContext (snd s)
                                  fszmq.Socket.send (fst s) msg }
@@ -124,12 +135,12 @@ module Agent =
         | e -> printfn "requester<%A,%A>: %A" typeof<'t> typeof<'u> e; raise e
       }
 
-  let startPuller<'t> (c:Context.T) (m:Machine) (port:int) : T<'t> = T<'t>.Start (Socket.receiver<'t> (Socket.pull c m port) None)
-  let startPusher<'t> (c:Context.T) (n:Network) (port:int) : T<'t> = T<'t>.Start (Socket.sender<'t> (Socket.push c n port) None)
-  let startSubscriber<'t> (c:Context.T) (m:Machine) (port:int) : T<'t> = T<'t>.Start (Socket.receiver<'t> (Socket.subscribe c m port) None)
-  let startPublisher<'t> (c:Context.T) (n:Network) (port:int) : T<'t> = T<'t>.Start (Socket.sender<'t> (Socket.publish c n port) None)
-  let startRequester<'t,'u> (c:Context.T) (m:Machine) (port:int) : T<Socket.ReqRep<'t,'u>> = T<Socket.ReqRep<'t,'u>>.Start (Socket.requester<'t,'u> (Socket.request c m port) None)
-  let startReplyer<'t,'u> (c:Context.T) (n:Network) (port:int) (callback:'t->'u Async) : T<Socket.ReqRep<'t,'u>> = T<Socket.ReqRep<'t,'u>>.Start (Socket.replyer<'t,'u> (Socket.reply c n port) None callback)
+  let startPuller<'t> (c:Context.T) (m:Connection.Machine) (port:int) : T<'t> = T<'t>.Start (Socket.receiver<'t> (Socket.pull c m port) None)
+  let startPusher<'t> (c:Context.T) (n:Connection.Network) (port:int) : T<'t> = T<'t>.Start (Socket.sender<'t> (Socket.push c n port) None)
+  let startSubscriber<'t> (c:Context.T) (m:Connection.Machine) (port:int) : T<'t> = T<'t>.Start (Socket.receiver<'t> (Socket.subscribe c m port) None)
+  let startPublisher<'t> (c:Context.T) (n:Connection.Network) (port:int) : T<'t> = T<'t>.Start (Socket.sender<'t> (Socket.publish c n port) None)
+  let startRequester<'t,'u> (c:Context.T) (m:Connection.Machine) (port:int) : T<Socket.ReqRep<'t,'u>> = T<Socket.ReqRep<'t,'u>>.Start (Socket.requester<'t,'u> (Socket.request c m port) None)
+  let startReplyer<'t,'u> (c:Context.T) (n:Connection.Network) (port:int) (callback:'t->'u Async) : T<Socket.ReqRep<'t,'u>> = T<Socket.ReqRep<'t,'u>>.Start (Socket.replyer<'t,'u> (Socket.reply c n port) None callback)
 
   let send<'t> (message:'t) (t:T<'t>) : unit = t.Post message
   let receive<'t> (t:T<'t>) : 't Async = t.Receive ()
--- src/test/Tests.fs
+++ src/test/Tests.fs
@@ -17,30 +17,30 @@ module Property =
   let context = FSzmq.Context.create ()
 
   let [<Test>] ``A message sent through an agent stays the same`` () =
-    let puller = FSzmq.Agent.startPuller context FSzmq.Machine.Localhost 12345
-    let pusher = FSzmq.Agent.startPusher context FSzmq.Network.Localhost 12345
+    let puller = FSzmq.Agent.startPuller context FSzmq.Connection.Localhost 12345
+    let pusher = FSzmq.Agent.startPusher context FSzmq.Connection.LocalhostNetwork 12345
     property (fun x -> pusher |> FSzmq.Agent.send x
                        puller |> FSzmq.Agent.receive |> Async.RunSynchronously = x)
 
   let [<Test>] ``A message sent through a publisher agent can be received`` () =
-    let subscriber = FSzmq.Agent.startSubscriber context FSzmq.Machine.Localhost 12346
-    let publisher = FSzmq.Agent.startPublisher context FSzmq.Network.Localhost 12346
+    let subscriber = FSzmq.Agent.startSubscriber context FSzmq.Connection.Localhost 12346
+    let publisher = FSzmq.Agent.startPublisher context FSzmq.Connection.LocalhostNetwork 12346
     Async.Sleep 2000 |> Async.RunSynchronously
     property (fun x -> publisher |> FSzmq.Agent.send x; true)
     property (fun x -> publisher |> FSzmq.Agent.send x; true)
     subscriber |> FSzmq.Agent.receive |> Async.RunSynchronously |> ignore
 
   let [<Test>] ``A message sent through a request agent gets the expected reply`` () =
-    let replyer = FSzmq.Agent.startReplyer context FSzmq.Network.Localhost 12348 (fun i -> async {return i + 1})
-    let requester = FSzmq.Agent.startRequester context FSzmq.Machine.Localhost 12348
+    let replyer = FSzmq.Agent.startReplyer context FSzmq.Connection.LocalhostNetwork 12348 (fun i -> async {return i + 1})
+    let requester = FSzmq.Agent.startRequester context FSzmq.Connection.Localhost 12348
     property (fun x -> requester |> FSzmq.Agent.request x |> Async.RunSynchronously = x + 1)
 
   let [<PerformanceTest>] ``Creating many agents will work easily`` () =
     let numAgents = 90 (* actually twice as much *)
     let agents =
       [0uy..(byte numAgents)]
-      |> List.map (fun i -> i, ( FSzmq.Agent.startPusher context FSzmq.Network.Localhost (int i + 12000)
-                               , FSzmq.Agent.startPuller context FSzmq.Machine.Localhost (int i + 12000) ))
+      |> List.map (fun i -> i, ( FSzmq.Agent.startPusher context FSzmq.Connection.LocalhostNetwork (int i + 12000)
+                               , FSzmq.Agent.startPuller context FSzmq.Connection.Localhost (int i + 12000) ))
       |> Map.ofList
     property (fun b x -> let (pusher, puller) = agents |> Map.find (byte (abs (b % numAgents)))
                          pusher |> FSzmq.Agent.send x
